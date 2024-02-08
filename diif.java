0a268a3a9f0c0b92d5a96900668caf839cbdc719
vivek raj <vivekraj19601@gmail.com>
2024-02-08 11:48:00 -0800

    add my name

--- diif.java
+++ diif.java
@@ -2,7 +2,9 @@ public class diif {
     public static void main(String[] args){
         int a = 10;
         int b = 20;
-        System.out.print("sum is "+(a-b));
+        System.out.print("sum is "+(a-b)); 
+        System.out.print("my name is vivek roy");
+
 
     }
 }
