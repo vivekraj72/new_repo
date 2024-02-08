b0eca5208f5bf9ada22234a91e7cc55385f0835b
vivek raj <vivekraj19601@gmail.com>
2024-02-08 10:55:12 -0800

    adding sum file

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum{
     public static void main(String[] args){
         int a = 10;
         int b = 20;
-        System.out.print(a+b);
+        System.out.print("sum is "+(a+b));
 
     }
 }
